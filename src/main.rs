cdc656748ca53494c957dffe625cc6cf63a2188b
Eleonora Kiziv <eleonora_kiziv@brown.edu>
2020-07-08 13:09:49 -0400

    Complete load generator

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,30 @@
 
 #[macro_use]
 extern crate slog;
-extern crate slog_term;
-extern crate rand;
 extern crate chrono;
+extern crate rand;
+extern crate slog_term;
 
-
-
+use chrono::Local;
+use chrono::NaiveDateTime;
+use fake::Fake;
+use noria::{DataType, SyncTable, SyncView};
+use noria::{SyncControllerHandle, ZookeeperAuthority};
 use rand::seq::SliceRandom;
-
-use std::io::{BufRead, BufReader};
 use slog::Drain;
 use slog::Logger;
 use slog_term::term_full;
-use std::fs::File;
-use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{Sender, Receiver};
-use std::sync::mpsc;
-use std::thread;
-use noria::{SyncControllerHandle, ZookeeperAuthority};
-use noria::{DataType, SyncTable};
-use fake::{Fake};
 use std::collections::HashMap;
-
-use chrono::Local;
+use std::convert::TryInto;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::io::{BufRead, BufReader};
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+use std::{thread, time};
 
 pub struct NoriaBackend {
     pub handle: SyncControllerHandle<ZookeeperAuthority, tokio::runtime::TaskExecutor>,
@@ -38,19 +39,17 @@ pub struct NoriaBackend {
 
 impl NoriaBackend {
     pub fn new() -> Result<NoriaBackend, std::io::Error> {
-        let  log = Logger::root(Mutex::new(term_full()).fuse(), o!());
+        let log = Logger::root(Mutex::new(term_full()).fuse(), o!());
         let zk_auth = ZookeeperAuthority::new("127.0.0.1:2181/hello")
-        .expect("failed to connect to Zookeeper");
+            .expect("failed to connect to Zookeeper");
 
         debug!(log, "Connecting to Noria...");
         let rt = tokio::runtime::Runtime::new().unwrap();
         let executor = rt.executor();
         let mut ch = SyncControllerHandle::new(zk_auth, executor.clone())
-        .expect("failed to connect to Noria controller");
+            .expect("failed to connect to Noria controller");
         println!("Looking for inputs");
-        let inputs = ch
-            .inputs()
-            .expect("couldn't get inputs from Noria");
+        let inputs = ch.inputs().expect("couldn't get inputs from Noria");
         println!("inputs: {:?}", inputs);
         Ok(NoriaBackend {
             handle: ch,
@@ -60,11 +59,12 @@ impl NoriaBackend {
     }
 }
 
-
 fn main() {
     // read table names
     let mut names: Vec<String> = Vec::new();
-    let buffered = BufReader::new(File::open("/Users/eleonorakiziv/rust/websubmit-rs/websubmit-rs/info.txt").unwrap());
+    let buffered = BufReader::new(
+        File::open("/Users/eleonorakiziv/rust/websubmit-rs/websubmit-rs/info.txt").unwrap(),
+    );
     for line in buffered.lines() {
         let name = line.unwrap().to_string();
         names.push(name.trim_matches('\"').to_string());
@@ -74,19 +74,16 @@ fn main() {
     }
 
     // instantiate Noria
-    let backend = Arc::new(Mutex::new(
-        NoriaBackend::new()
-        .unwrap()
-    ));
+    let backend = Arc::new(Mutex::new(NoriaBackend::new().unwrap()));
 
-    let size: u64 = names.len() as u64;
-    let qids: Vec<u64> = (0..size).collect();
-    let num_requests = 50;
+    let qids: Vec<u64> = (0..1000).collect();
+    let num_requests = 1000;
 
     // fetch table handlers
     let mut table_handlers: HashMap<String, SyncTable> = {
         let mut bg = backend.lock().unwrap();
-        names.clone()
+        names
+            .clone()
             .into_iter()
             .map(|e| {
                 let table_name = format!("answers_{}", e.clone());
@@ -97,49 +94,57 @@ fn main() {
     };
 
     // fetch view
-    println!("here are table_handlers: {:?}", table_handlers.clone());
+    let mut view: SyncView = {
+        let mut bg = backend.lock().unwrap();
+        bg.handle
+            .view("answers_by_q_and_emailkey")
+            .unwrap()
+            .into_sync()
+    };
 
     let mut threads = Vec::new();
     let (tx, rx) = mpsc::channel();
-    let wid = thread::spawn(move || {
-        write(tx, names, qids, &backend, num_requests, &mut table_handlers)
-    });
+    let wid = thread::spawn(move || write(tx, names, qids, num_requests, &mut table_handlers));
 
     threads.push(wid);
 
-    // let rid = thread::spawn(move || {
-    //     read(rx)
-    // });
-    // threads.push(rid, num_rq);
-
-    // one thread for reading
+    let rid = thread::spawn(move || read(rx, num_requests, &mut view));
+    threads.push(rid);
 
     // waiting for both threads to finish
     for thread in threads {
         thread.join().expect("oops! the child thread panicked");
     }
 
-
+    let mut users_view: SyncView = {
+        let mut bg = backend.lock().unwrap();
+        bg.handle.view("all_users").unwrap().into_sync()
+    };
+    let res = users_view.lookup(&[(0 as u64).into()], true).unwrap();
+    assert_eq!(res.len(), 50);
 }
 
 fn write(
     tx: Sender<(String, u64)>,
     names: Vec<String>,
     qids: Vec<u64>,
-    backend: &Arc<Mutex<NoriaBackend>>,
     num_rq: u64,
-    handlers: &mut HashMap<String, SyncTable>
-    ) {
+    handlers: &mut HashMap<String, SyncTable>,
+) {
     use fake::faker::lorem::en::*;
     let mut i = 0;
-    while i < num_rq {
-
-    // pick a random user, write to its table
+    while i <= num_rq {
+        let start = Instant::now();
+        // pick a random user, write to its table
         let name = names.choose(&mut rand::thread_rng()).unwrap();
         let qid: &u64 = qids.choose(&mut rand::thread_rng()).unwrap();
         let answer: String = Sentence(5..7).fake();
-        let timestamp = Local::now().naive_local();
-        let ts: DataType = DataType::Timestamp(timestamp);
+
+        // record the time since the previous request
+        let new_ts = Local::now().naive_local();
+        println!("{:?}", new_ts);
+        let ts: DataType = DataType::Timestamp(new_ts);
+
         let rec: Vec<DataType> = vec![
             (*name).clone().into(),
             0.into(),
@@ -148,11 +153,81 @@ fn write(
             ts.into(),
         ];
         let table = handlers.get_mut(name).unwrap();
-        (table).insert(rec).expect("failed to insert into answers table");
-        println!("Inserted");
+        (table)
+            .insert(rec)
+            .expect("failed to insert into answers table");
+        // let to_sleep =
+        //     time::Duration::new(0, 100_000_000).checked_sub(Instant::now().duration_since(start));
+        // if to_sleep.is_some() {
+        //     thread::sleep(to_sleep.unwrap());
+        // }
         let tuple = ((*name).clone(), (*qid).clone());
         tx.send(tuple).unwrap();
         i += 1;
     }
 }
 
+fn read(rx: Receiver<(String, u64)>, num_rq: u64, view: &mut SyncView) {
+    let mut end_times: Vec<(NaiveDateTime, NaiveDateTime)> = Vec::new();
+    let mut i = 0;
+
+    while i <= num_rq {
+        if i == 200 {
+            println!("!!!!!!!!!!!!!NOW!!!!!!!!!!!!!!!!")
+        }
+        let (email, q) = rx.recv().unwrap();
+        let mut res = Vec::new();
+
+        while res.len() < 1 {
+            res = view
+                .lookup(&[email.clone().into(), q.into()], true)
+                .expect("failed to look up the user in answers_by_q_and_emailkey");
+        }
+        res //
+            .into_iter()
+            .map(|r| {
+                if let DataType::Timestamp(ts) = r[4] {
+                    Some(ts)
+                } else {
+                    None
+                }
+            })
+            .for_each(|ts| {
+                let times = (ts.unwrap(), Local::now().naive_local());
+                end_times.push(times);
+            });
+
+        i += 1;
+    }
+    println!("Done!");
+
+    end_times.sort_by_key(|k| k.0);
+
+    let mut latency_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open("latency.txt")
+        .unwrap();
+    let mut interval_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open("intervals.txt")
+        .unwrap();
+
+    let mut prev = end_times[0].0;
+    println!("length of end_times: {:?}", end_times.len());
+    for (start, end) in end_times.into_iter() {
+        let interval = start.signed_duration_since(prev).num_milliseconds();
+        if interval < 0 {
+            println!(
+                "time since the last element is negative. start: {:?}, prev: {:?}",
+                start.clone(),
+                prev.clone()
+            );
+        }
+        prev = start;
+        let latency = end.signed_duration_since(start).num_milliseconds();
+        let _err = write!(&mut latency_file, "{}\n", latency);
+        let _err = write!(&mut interval_file, "{}\n", interval);
+    }
+}
